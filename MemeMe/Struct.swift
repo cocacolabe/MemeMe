e569a5aa2d5c9cfadbbe1302da75ee722c98ce89
Ginne Chou <cocacolabe@users.noreply.github.com>
2017-02-17 08:47:36 -0500

    struct file

---
+++ MemeMe/Struct.swift
@@ -0,0 +1,18 @@
+//
+//  Struct.swift
+//  MemeMe
+//
+//  Created by Ting Chun Chou on 2/15/17.
+//  Copyright © 2017 TingChunChou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    var topTextField: String
+    var bottomTextField: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
